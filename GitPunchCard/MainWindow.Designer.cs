f250abd8d6284ac0d6bbd25791847f280b2afacc
WiseClock <kuanghao04@gmail.com>
2017-12-15 01:01:29 -0800

    initial version.

---
+++ GitPunchCard/MainWindow.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace GitPunchCard
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pnlPicture = new System.Windows.Forms.Panel();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pnlPicture.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(412, 10);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(12, 12);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(394, 20);
+            this.txtLocation.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(493, 10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pnlPicture
+            // 
+            this.pnlPicture.AutoScroll = true;
+            this.pnlPicture.Controls.Add(this.picResult);
+            this.pnlPicture.Location = new System.Drawing.Point(12, 38);
+            this.pnlPicture.Name = "pnlPicture";
+            this.pnlPicture.Size = new System.Drawing.Size(855, 575);
+            this.pnlPicture.TabIndex = 3;
+            // 
+            // picResult
+            // 
+            this.picResult.Location = new System.Drawing.Point(3, 3);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(539, 350);
+            this.picResult.TabIndex = 0;
+            this.picResult.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(574, 10);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 625);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pnlPicture);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "MainWindow";
+            this.Text = "Git Punch Card";
+            this.pnlPicture.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel pnlPicture;
+        private System.Windows.Forms.PictureBox picResult;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ GitPunchCard/MainWindow.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibGit2Sharp;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace GitPunchCard
+{
+    public partial class MainWindow : Form
+    {
+        private readonly Font _mediumFont = new Font("Arial", 12, FontStyle.Regular, GraphicsUnit.Pixel);
+        private readonly Font _largeFont = new Font("Arial", 16, FontStyle.Bold, GraphicsUnit.Pixel);
+        private readonly Dictionary<string, string> _emailStringPair = new Dictionary<string, string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private static bool CheckIsRepo(string path)
+        {
+            return Repository.IsValid(path);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!CheckIsRepo(txtLocation.Text))
+                return;
+            string path = txtLocation.Text;
+
+            _emailStringPair.Clear();
+            Dictionary<string, Dictionary<string, int>> container = new Dictionary<string, Dictionary<string, int>>();
+            int maxCommits = 0;
+
+            using (Repository repo = new Repository(path))
+            {
+                IEnumerable<Commit> commits = repo.Head.Commits;
+                foreach (var commit in commits)
+                {
+                    string user = commit.Committer.Email;
+                    // if (!_emailStringPair.ContainsKey(user))
+                        _emailStringPair[user] = commit.Committer.Name;
+                    int dayOfWeek = (int)commit.Committer.When.DayOfWeek; // starts from sunday
+                    int hour = commit.Committer.When.Hour;
+                    string dayHourKey = $"{dayOfWeek}_{hour}";
+                    if (container.ContainsKey(user))
+                    {
+                        if (container[user].ContainsKey(dayHourKey))
+                        {
+                            container[user][dayHourKey]++;
+                            if (container[user][dayHourKey] > maxCommits)
+                                maxCommits = container[user][dayHourKey];
+                        }
+                        else
+                        {
+                            container[user][dayHourKey] = 1;
+                        }
+                    }
+                    else
+                    {
+                        container[user] = new Dictionary<string, int> { { dayHourKey, 1 } };
+                    }
+                }
+            }
+
+            DrawAllUsers(container, maxCommits);
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (CommonOpenFileDialog dialog = new CommonOpenFileDialog { IsFolderPicker = true })
+            {
+                if (dialog.ShowDialog() != CommonFileDialogResult.Ok) return;
+                if (CheckIsRepo(dialog.FileName))
+                {
+                    txtLocation.Text = dialog.FileName;
+                }
+                else
+                {
+                    MessageBox.Show("Not a .git folder", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void DrawAllUsers(Dictionary<string, Dictionary<string, int>> container, int maxCommits)
+        {
+            const int width = 820;
+            int height = 5000;
+
+            int yOffset = 0;
+
+            using (Bitmap bmp = new Bitmap(width, height))
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.TextRenderingHint = TextRenderingHint.AntiAlias;
+                g.Clear(Color.White);
+
+                foreach (var namePair in container)
+                {
+                    Dictionary<string, int> sortedValue = namePair.Value.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+                    DrawUser(g, ref yOffset, namePair.Key, sortedValue, maxCommits);
+                }
+
+                height = yOffset;
+                picResult.Size = new Size(width, height);
+
+                Bitmap bitmapCrop = new Bitmap(width, height);
+                using (Graphics gCrop = Graphics.FromImage(bitmapCrop))
+                {
+                    gCrop.DrawImage(bmp, new Rectangle(0, 0, width, height), new Rectangle(0, 0, width, height), GraphicsUnit.Pixel);
+                }
+                picResult.Image = new Bitmap(bitmapCrop, width, height);
+            }
+        }
+
+        private void DrawUser(Graphics g, ref int yOffset, string name, Dictionary<string, int> hours, int maxCommits)
+        {
+            g.DrawString(_emailStringPair[name], _largeFont, Brushes.DimGray, 0, yOffset);
+            yOffset += 40;
+
+            for (int day = 0; day < 7; day++)
+            {
+                using (StringFormat format = new StringFormat())
+                {
+                    format.LineAlignment = StringAlignment.Center;
+                    g.DrawString(GetDayString(day), _mediumFont, Brushes.DimGray, 0, yOffset, format);
+                }
+                
+                g.DrawLine(Pens.LightGray, 0, yOffset + 40, 820, yOffset + 40);
+                int x = 100;
+                for (int hour = 0; hour < 24; hour++)
+                {
+                    g.DrawLine(Pens.LightGray, x, yOffset + 25 + (hour % 2 == 1 ? 5 : 0), x, yOffset + 40);
+
+                    // draw dot
+                    string key = $"{day}_{hour}";
+                    if (hours.ContainsKey(key))
+                    {
+                        float sizePercentage = hours[key] / (float)maxCommits;
+                        float size = sizePercentage * 29 + 3; // 29 max, 3 base
+                        g.FillEllipse(new SolidBrush(Color.FromArgb(60, 60, 60)), x - size / 2, yOffset - size / 2, size, size);
+                    }
+
+                    x += 30;
+                }
+
+                yOffset += 60;
+            }
+
+            int timeX = 100;
+            for (int hour = 0; hour < 24; hour++)
+            {
+                using (StringFormat format = new StringFormat())
+                {
+                    format.Alignment = StringAlignment.Center;
+                    format.LineAlignment = StringAlignment.Far;
+                    g.DrawString(hour.ToString(), _mediumFont, Brushes.DimGray, timeX, yOffset + 5, format);
+                }
+                timeX += 30;
+            }
+
+            yOffset += 15;
+        }
+
+        private static string GetDayString(int day)
+        {
+            switch (day)
+            {
+                case 0:
+                    return "Sunday";
+                case 1:
+                    return "Monday";
+                case 2:
+                    return "Tuesday";
+                case 3:
+                    return "Wednesday";
+                case 4:
+                    return "Thursday";
+                case 5:
+                    return "Friday";
+                case 6:
+                    return "Saturday";
+                default:
+                    return "";
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (picResult.Image == null)
+                return;
+
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.DefaultExt = "png";
+                sfd.AddExtension = true;
+                sfd.Filter = "PNG|*.png";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    picResult.Image.Save(sfd.FileName, ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
